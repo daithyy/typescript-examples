8aed7be6931c816d85d141e19ab8aa7ee7fa0a72
David Brennan <david.brennan@mail.itsligo.ie>
2018-02-14 12:17:59 +0000

    Done Question 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  fullName : string = 'David Brennan';
+  fullNameMsg : string;
+
+
+  outputFullName() : void {
+    console.log("Hello", this.fullName);
+  }
+
+  outputFNameSName(fName : string, sName : string) : void {
+    console.log(fName);
+    console.log(sName);
+    this.fullNameMsg = fName + " " + sName;
+    console.log("Welcome ", this.fullNameMsg);
+  }
 }
